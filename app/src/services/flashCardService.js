003b958f6325e50081cf8e7dfa4fce08b6f10f28
paras1478 <pankaj147878@gmail.com>
2026-02-09 01:36:36 +0530

    update frontend

--- app/src/services/flashCardService.js
+++ app/src/services/flashCardService.js
@@ -2,70 +2,32 @@ import axiosInstance from "../utils/axiosInstance";
 import { API_PATHS } from "../utils/apiPaths";
 
 const getAllFlashcardSets = async () => {
-  try {
-    const response = await axiosInstance.get(
-      API_PATHS.FLASHCARDS.GET_ALL_FLASHCARD_SETS
-    );
-    return response.data.data || [];
-  } catch (error) {
-    throw error.response?.data || {
-      message: "Failed to fetch flashcard sets",
-    };
-  }
+  const res = await axiosInstance.get(
+    API_PATHS.FLASHCARDS.GET_ALL_FLASHCARD_SETS
+  );
+  return res.data.data || [];
 };
 
 const getFlashcardsForDocument = async (documentId) => {
-  try {
-    const response = await axiosInstance.get(
-      API_PATHS.FLASHCARDS.GET_FLASHCARDS_FOR_DOC(documentId)
-    );
-
-    return Array.isArray(response.data.data)
-      ? response.data.data
-      : [];
-  } catch (error) {
-    throw error.response?.data || {
-      message: "Failed to fetch flashcards",
-    };
-  }
+  const res = await axiosInstance.get(
+    API_PATHS.FLASHCARDS.GET_FLASHCARDS_FOR_DOC(documentId)
+  );
+  return res.data.data || [];
 };
+
 const generateFlashcards = async (documentId, count = 10) => {
-  try {
-    const response = await axiosInstance.post(
-      API_PATHS.AI.GENERATE_FLASHCARDS,
-      { documentId, count }
-    );
-
-    if (!response.data?.success) {
-      throw new Error(
-        response.data?.message || "AI could not generate flashcards"
-      );
-    }
-
-    return response.data.data;
-  } catch (error) {
-    throw (
-      error.response?.data || {
-        message: error.message || "Unable to generate flashcards",
-      }
-    );
-  }
+  const res = await axiosInstance.post(API_PATHS.AI.GENERATE_FLASHCARDS, {
+    documentId,
+    count,
+  });
+  return res.data.data;
 };
 
+const toggleStar = async (cardId) =>
+  axiosInstance.put(API_PATHS.FLASHCARDS.TOGGLE_STAR(cardId));
 
-const toggleStar = async (cardId) => {
-  const response = await axiosInstance.put(
-    API_PATHS.FLASHCARDS.TOGGLE_STAR(cardId)
-  );
-  return response.data;
-};
-
-const deleteFlashcardSet = async (id) => {
-  const response = await axiosInstance.delete(
-    API_PATHS.FLASHCARDS.DELETE_FLASHCARD_SET(id)
-  );
-  return response.data;
-};
+const deleteFlashcardSet = async (id) =>
+  axiosInstance.delete(API_PATHS.FLASHCARDS.DELETE_FLASHCARD_SET(id));
 
 export default {
   getAllFlashcardSets,
