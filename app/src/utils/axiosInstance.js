5b75028cec0e842c32e9fc27e5ec4753a044111a
paras1478 <pankaj147878@gmail.com>
2026-02-09 18:43:24 +0530

    fix: frontend cod

--- app/src/utils/axiosInstance.js
+++ app/src/utils/axiosInstance.js
@@ -1,19 +1,31 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
- baseURL: "/api", 
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 80000,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(
+      error.response?.data || { message: "Network error" }
+    );
   }
-  return config;
-});
+);
 
 export default axiosInstance;
